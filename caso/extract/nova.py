665867db9366785a0b986b700e118889d4c39a9e
Aida Palacio Hoz <aidaph@ifca.unican.es>
2020-07-07 14:38:53 +0200

    V1.4: Get Public IPs count

--- caso/extract/nova.py
+++ caso/extract/nova.py
@@ -50,10 +50,6 @@ class OpenStackExtractor(base.BaseExtractor):
         session = keystone_client.get_session(CONF, project)
         return glanceclient.client.Client(2, session=session)
 
-    def _get_neutron_client(self, project):
-        session = keystone_client.get_session(CONF, project)
-        return neutronclient.v2_0.client.Client(session=session)
-
     def build_record(self, server, vo, images, flavors, users):
         server_start = self._get_server_start(server)
         server_end = self._get_server_end(server)
@@ -78,6 +74,9 @@ class OpenStackExtractor(base.BaseExtractor):
         else:
             bench_name = bench_value = None
             memory = cpu_count = disk = None
+
+        for name, value in server.addresses.items():
+             public_ips = [i for i in value if i["OS-EXT-IPS:type"] == "floating"]
 
         if not all([bench_name, bench_value]):
             if any([bench_name, bench_value]):
@@ -106,7 +105,8 @@ class OpenStackExtractor(base.BaseExtractor):
                                benchmark_value=bench_value,
                                memory=memory,
                                cpu_count=cpu_count,
-                               disk=disk)
+                               disk=disk,
+                               public_ip_count = len(public_ips))
 
         return r
 
