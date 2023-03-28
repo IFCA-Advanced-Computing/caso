47b284fa98e0dd3bef3c8060fd9b1a13c5d1771d
Alvaro Lopez Garcia <aloga@ifca.unican.es>
2023-03-28 11:26:23 +0200

    Revert "Do not use bandit here"
    This reverts commit ca12fc29ccc7379114660b8fffaaf0033cea9682.

--- caso/messenger/ssm.py
+++ caso/messenger/ssm.py
@@ -18,11 +18,11 @@ import json
 import typing
 import warnings
 
+import defusedxml.ElementTree as ET
 import dirq.QueueSimple
 from oslo_config import cfg
 from oslo_log import log
 import six
-import xml.etree.ElementTree as ET
 
 import caso.exception
 import caso.messenger
